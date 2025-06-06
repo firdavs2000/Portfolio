71766cbe02b02143f5cb6cc38f85c41dcda055f3
Firdavs Komilov <firdavskomilov05@gmail.com>
2025-06-06 21:07:30 +0500

    first commit

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -29,15 +29,23 @@ const projects = [
 const Project = () => {
   const { darkMode } = useContext(DarkModeContext);
 
+  // Ref for prev and next buttons
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
   return (
     <section className={`project ${darkMode ? 'dark' : 'light'}`}>
       <div className="container">
         <h2 className="title">Projects</h2>
+
         <Swiper
           modules={[Navigation, Pagination, Autoplay]}
           spaceBetween={20}
           slidesPerView={3}
-          navigation
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
           pagination={{ clickable: true }}
           autoplay={{ delay: 3000, disableOnInteraction: false }}
           breakpoints={{
@@ -45,16 +53,33 @@ const Project = () => {
             640: { slidesPerView: 2 },
             1024: { slidesPerView: 3 },
           }}
+          onBeforeInit={(swiper) => {
+            // Manually assign navigation elements because refs are initially null
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+          }}
           className="project_box"
         >
           {projects.map((project, index) => (
             <SwiperSlide key={index}>
               <div className="project_card">
-                <img src={project.img} alt={`${project.name} preview`} className="project_icon" />
+                <img
+                  src={project.img}
+                  alt={`${project.name} preview`}
+                  className="project_icon"
+                />
                 <h3 className="project_text">{project.name}</h3>
               </div>
             </SwiperSlide>
           ))}
+
+          {/* Custom Prev/Next buttons */}
+          <div ref={prevRef} className="swiper-button-prev">
+            ‹
+          </div>
+          <div ref={nextRef} className="swiper-button-next">
+            ›
+          </div>
         </Swiper>
       </div>
     </section>
@@ -62,3 +87,4 @@ const Project = () => {
 };
 
 export default Project;
+
